39203b4b375cd00387f94d45d3a7cd55f059e018
Nuno Henriques <nuno.henriques@izertislabs.pt>
2025-01-17 11:51:06 +0000

    Link, NavLink, custom 404 page, jobs page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobPage from './pages/JobPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<MainLayout />}>
             <Route index element={<HomePage />} />
-            <Route path="/jobs/:id" element={<JobPage />} />
+            <Route path="/jobs" element={<JobPage />} />
+            <Route path="*" element={<NotFoundPage />} /> {/* 404 Not Found, the * represents any path */}
         </Route>
     )
 );
