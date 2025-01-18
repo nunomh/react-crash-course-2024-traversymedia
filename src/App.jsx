5afb9735b6ccaa33455e02c4e9ed79722cee3af9
Nuno Henriques <nuno.henriques@izertislabs.pt>
2025-01-18 18:42:15 +0000

    Add proxy to server and start creating single job page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
-import JobPage from './pages/JobPage';
+import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
+import JobPage from './pages/JobPage';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<MainLayout />}>
             <Route index element={<HomePage />} />
-            <Route path="/jobs" element={<JobPage />} />
+            <Route path="/jobs" element={<JobsPage />} />
+            <Route path="/jobs/:id" element={<JobPage />} />
             <Route path="*" element={<NotFoundPage />} /> {/* 404 Not Found, the * represents any path */}
         </Route>
     )
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,11 +1,5 @@
-import JobListings from '../components/JobListings';
-
 const JobPage = () => {
-    return (
-        <section className="bg-blue-50 px-4 py-6">
-            <JobListings />
-        </section>
-    );
+    return <div>JobPage</div>;
 };
 
 export default JobPage;
