f8f66cfa594405560d76bc9ce7dd0e21b960e01e
Nuno Henriques <nuno.henriques@izertislabs.pt>
2025-01-20 17:26:23 +0000

    Finish tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 
 const App = () => {
     // Add New Job
@@ -29,6 +30,18 @@ const App = () => {
         return;
     };
 
+    // Update Job
+    const updateJob = async updatedJob => {
+        const res = await fetch(`/api/jobs/${updatedJob.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updatedJob),
+        });
+        return;
+    };
+
     const router = createBrowserRouter(
         createRoutesFromElements(
             <Route path="/" element={<MainLayout />}>
@@ -36,6 +49,7 @@ const App = () => {
                 <Route path="/jobs" element={<JobsPage />} />
                 <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
                 <Route path="/jobs/:id" element={<JobPage deleteJob={deleteJob} />} loader={jobLoader} />
+                <Route path="/edit-job/:id" element={<EditJobPage updateJobSubmit={updateJob} />} loader={jobLoader} />
                 <Route path="*" element={<NotFoundPage />} /> {/* 404 Not Found, the * represents any path */}
             </Route>
         )
